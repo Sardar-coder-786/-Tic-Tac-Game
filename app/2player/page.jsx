09703b6997675774b60f005c82755e5204e810d6
Sardar-coder-786 <usman1612887@gmail.com>
2025-03-03 05:18:45 +0500

    .

--- app/2player/page.jsx
+++ app/2player/page.jsx
@@ -1,7 +1,6 @@
 "use client";
 import Link from "next/link";
 import { useState, useEffect } from "react";
-import FbLink from "../component/fblink";
 
 function App() {
   let [turnO, setturnO] = useState(true);
@@ -86,7 +85,9 @@ function App() {
         🔄 Restart Game
       </button>
       {/* Footer */}
-      <FbLink/>
+      <p className="text-sm mt-6 text-gray-400">
+        Made with ❤️ by <Link href="https://www.facebook.com/usmankhan.786.facebook" className="text-blue-400 underline">USMAN KHAN</Link>
+      </p>
     </div>
   );
 }
--- app/page.jsx
+++ app/page.jsx
@@ -1,5 +1,4 @@
 import Link from "next/link";
-import FbLink from "./component/fblink";
 
 function App() {
   return ( 
@@ -38,7 +37,9 @@ function App() {
       </div>
 
       {/* Footer */}
-      <FbLink/>
+      <p className="text-sm mt-6 text-gray-400">
+        Made with ❤️ by <Link href="https://www.facebook.com/usmankhan.786.facebook" className="text-blue-400 underline">USMAN KHAN</Link>
+      </p>
 
     </div>
   );
